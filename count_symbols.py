94cc2df2df9d1caa65ff0b2cc5125e84941b7a89
akhaietskyi <akhaietskyi@themeetgroup.com>
2019-04-29 03:44:13 +0300

    Fixed warnings

--- count_symbols.py
+++ count_symbols.py
@@ -1,15 +1,16 @@
-_author_ = "Andrii Khaietskyi"
-_status_ = "Draft"
-_copyright_ = "Copyright 2019"
-
-"""
-Simple program to count non space symbols in the file
+""" Simple program to count non space symbols in the file
+    Usage: python count_symbols.py <filename> <columns>
 """
 
 import re
 import sys
 
-def load_file( filename ):
+_AUTHOR_ = "Andrii Khaietskyi"
+_STATUS_ = "Draft"
+_COPYRIGHT_ = "Copyright 2019"
+
+def load_file(filename):
+    """load file and return array of lines"""
     try:
         text_file = open(filename, "r")
         lines = text_file.readlines()
@@ -19,40 +20,42 @@ def load_file( filename ):
     except IOError:
         print "Error: File '" + filename + "'' doesn't exist or access denied"
 
-def count_symbols( lines ):
+def count_symbols(lines):
+    """looks through each line and calculate non-space symbols"""
     symbols = {}
     for line in lines:
         for j in line:
-            if re.search("\S",j):   #ignore space symbols
+            if re.search(r"\S", j):   #ignore space symbols
                 if j in symbols:
                     symbols[j] += 1
                 else:
                     symbols[j] = 1
     return symbols
 
-def fancy_out( dict, columns ):
-    if not dict:
+def fancy_out(sym_dict, cols):
+    """output formated result"""
+    if not sym_dict:
         print "File has no symbols"
     else:
         print "The file contains next symbols and their quantity:"
-        lines = len(dict)
+        lines = len(sym_dict)
         total = 0
-        n = 0
-        for key in sorted(dict.keys()):
-            print "{:<9}".format(key + ":" + str(dict[key])),
-            total += dict[key]
-            n += 1
-            if n % columns == 0:
+        cur_col = 0
+        for key in sorted(sym_dict.keys()):
+            print "{:<9}".format(key + ":" + str(sym_dict[key])),
+            total += sym_dict[key]
+            cur_col += 1
+            if cur_col % cols == 0:
                 print
         print
         print "Number of different symbols:", lines
         print "Total number of symbols:", total
 
 if len(sys.argv) >= 2:
-    lines = load_file(sys.argv[1])
+    LINES = load_file(sys.argv[1])
 else:
     print "Please specify filename:", sys.argv[0], "<filename>"
 
-symbols_dict = count_symbols(lines)
-columns = int(sys.argv[2]) if len(sys.argv) >= 3 else 10
-fancy_out(symbols_dict, columns)
+SYMBOLS_DICT = count_symbols(LINES)
+COLUMNS = int(sys.argv[2]) if len(sys.argv) >= 3 else 10
+fancy_out(SYMBOLS_DICT, COLUMNS)
